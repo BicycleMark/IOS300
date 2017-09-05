54e197512567a8f713a904f52807d44f5e56a5d0
Adam Patridge <patridge@gmail.com>
2017-09-05 11:41:49 -0600

    Initial course exercises import.

---
+++ Exercise 2/Completed/MonkeyInfo/ViewController.designer.cs
@@ -0,0 +1,46 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace MonkeyInfo
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.NSLayoutConstraint constLeftText { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.NSLayoutConstraint constTopText { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIImageView imgMonkey { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (constLeftText != null) {
+                constLeftText.Dispose ();
+                constLeftText = null;
+            }
+
+            if (constTopText != null) {
+                constTopText.Dispose ();
+                constTopText = null;
+            }
+
+            if (imgMonkey != null) {
+                imgMonkey.Dispose ();
+                imgMonkey = null;
+            }
+        }
+    }
+}
---
+++ Exercise 3/Completed/MonkeyInfo/ViewController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Foundation;
+using UIKit;
+
+namespace MonkeyInfo
+{
+    public partial class ViewController : UIViewController
+    {
+        bool monkeyIsLeft = true;
+
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        void AnimateImage ()
+        {
+            View.RemoveConstraint(constMonkeyEdge);
+
+            monkeyIsLeft = !monkeyIsLeft;
+
+            var edge = monkeyIsLeft ? NSLayoutAttribute.Leading : NSLayoutAttribute.Trailing;
+
+            constMonkeyEdge = GetConstraint(constraint: constMonkeyEdge,
+                                            attribute1: edge,
+                                            attribute2: edge,
+                                            constant: constMonkeyEdge.Constant * -1);
+
+            this.View.AddConstraint(constMonkeyEdge);
+
+            UIView.Animate(1.5, () => View.LayoutIfNeeded());
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            imgMonkey.UserInteractionEnabled = true;
+
+            imgMonkey.AddGestureRecognizer(new UITapGestureRecognizer(AnimateImage));
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        public override void WillRotate (UIInterfaceOrientation toInterfaceOrientation, double duration)
+        {
+            base.WillRotate(toInterfaceOrientation, duration);
+
+            SetOrientation(toInterfaceOrientation == UIInterfaceOrientation.LandscapeLeft ||
+                           toInterfaceOrientation == UIInterfaceOrientation.LandscapeRight);
+        }
+
+        void SetOrientation(bool isLandscape)
+        {
+			//remove the constraints we want to change
+			this.View.RemoveConstraint(constLeftText);
+			this.View.RemoveConstraint(constTopText);
+
+			if (isLandscape == true)
+			{
+				constLeftText = GetConstraint(constraint: constLeftText,
+				   object2: imgMonkey,
+				   attribute2: NSLayoutAttribute.Trailing
+				);
+
+				constTopText = GetConstraint(constraint: constTopText,
+				   object2: this.View,
+				   attribute2: NSLayoutAttribute.Top
+				);
+			}
+			else
+			{
+				constLeftText = GetConstraint(constraint: constLeftText,
+				   object2: this.View,
+				   attribute2: NSLayoutAttribute.Leading
+				);
+
+				constTopText = GetConstraint(constraint: constTopText,
+				   object2: imgMonkey,
+				   attribute2: NSLayoutAttribute.Bottom
+				);
+			}
+
+			this.View.AddConstraint(constLeftText);
+			this.View.AddConstraint(constTopText);       
+        }
+
+		//helper method to create constraints based on existing constraints
+		NSLayoutConstraint GetConstraint(
+		   NSLayoutConstraint constraint,
+		   NSObject object1 = null,
+		   NSLayoutAttribute? attribute1 = null,
+		   NSLayoutRelation? relation = null,
+		   NSObject object2 = null,
+		   NSLayoutAttribute? attribute2 = null,
+		   nfloat? multiplier = null,
+		   nfloat? constant = null)
+		{
+			if (constraint == null)
+				return null;
+			return NSLayoutConstraint.Create(
+					 object1 ?? constraint.FirstItem,
+					 (attribute1 == null) ? constraint.FirstAttribute : attribute1.Value,
+					 (relation == null) ? constraint.Relation : relation.Value,
+					 object2 ?? constraint.SecondItem,
+					 (attribute2 == null) ? constraint.SecondAttribute : attribute2.Value,
+					 (multiplier == null) ? constraint.Multiplier : multiplier.Value,
+					 (constant == null) ? constraint.Constant : constant.Value);
+		}
+    }
+}
